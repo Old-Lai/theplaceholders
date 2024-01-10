f6b68f500826eaa4e65b59550a9fb59c6021d8f4
Beasted179 <clarios179@gmail.com>
2024-01-10 14:12:16 -0800

    added navigation back button

--- src/pages/ChrisPage.jsx
+++ src/pages/ChrisPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import BackButton from '../components/BackButton'
 function ChrisPage() {
   return (
     <div>
+      <BackButton />
       <h1>Chris's Page</h1>
       {/* Additional content goes here */}
     </div>
--- src/pages/HenryPage.jsx
+++ src/pages/HenryPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import BackButton from '../components/BackButton'
 function HenryPage() {
   return (
     <div>
+      <BackButton /> 
       <h1>Henry's Page</h1>
       {/* Additional content goes here */}
     </div>
