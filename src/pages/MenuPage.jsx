18255ac89a108c134c1f172c275e86ab684c7269
Beasted179 <clarios179@gmail.com>
2024-01-09 17:34:21 -0800

    bones started

---
+++ src/pages/MenuPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/MenuPage.css';
+function MenuPage() {
+  return (
+    <div className='groupTitle'>
+      <h1>The Placeholders</h1>
+    </div>
+  );
+}
+
+export default MenuPage;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as MenuPage } from './MenuPage';
+export { default as ChrisPage } from './ChrisPage';
+export { default as HenryPage } from './HenryPage';
+export { default as NotFoundPage } from './NotFoundPage';
