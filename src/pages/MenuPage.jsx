24958dfaea099793603e18ec80e0fbf6db1b7a34
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-01-11 17:42:22 -0800

    added blur to mainPage

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -3,14 +3,16 @@ import FloatingWords from "../components/FloatingWords";
 import '../styles/menuPage.sass';
 function MenuPage() {
   return (
-    <div className='menuBody'>
+    <>
       <FloatingWords />
-      <h1>The Placeholders</h1>
-      <ul className='menuLinks'>
-        <li className='chrisButton'><a href="/chris">Chris</a></li>
-        <li><a href="/oldlai">Henry</a></li>
-      </ul>
-    </div>
+      <div className='menuBody'>
+        <h1>The Placeholders</h1>
+        <ul className='menuLinks'>
+          <li className='chrisButton'><a href="/chris">Chris</a></li>
+          <li><a href="/oldlai">Henry</a></li>
+        </ul>
+      </div>
+    </>
   );
 }
 
