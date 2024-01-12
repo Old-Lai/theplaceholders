2ef7026b4619cc6ba32cb5caab58c9cd65e941d7
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-01-11 17:18:56 -0800

    moved floating words background to MenuPage.jsx (allowing calling of floatin words where needed instead of by default)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MenuPage, ChrisPage, HenryPage, NotFoundPage } from "./pages";
-import FloatingWords from "./components/FloatingWords";
+import { MenuPage, ChrisPage, OldlaiPage, NotFoundPage } from "./pages";
 function App() {
   return (
     <div>
-      <FloatingWords />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MenuPage />} />
           <Route path="/chris" element={<ChrisPage />} />
-          <Route path="/henry" element={<HenryPage />} />
+          <Route path="/oldlai" element={<OldlaiPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/oldlai/OldlaiPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BackButton from '../../components/BackButton'
+function OldlaiPage() {
+  return (
+    <div>
+      <BackButton /> 
+      <h1>Henry's Page</h1>
+      {/* Additional content goes here */}
+    </div>
+  );
+}
+
+export default OldlaiPage;
