447a5e57b7ad30fbcc781d90cd7921cf013fca82
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-01-22 20:37:34 -0800

    fixed array.map individual elements should have a unique key (replaced index with uuid)

--- src/components/FloatingWords.jsx
+++ src/components/FloatingWords.jsx
@@ -3,11 +3,13 @@ import '../styles/FloatingWords.css';
 
 const words = ['Innovative', 'Creative', 'Team Players', 'Problem Solvers', 'Analytical'];
 
+import { v4 as uuidv4 } from 'uuid';
+
 const FloatingWords = () => {
   return (
     <div className="floating-words-container">
-      {words.map((word, index) => (
-        <span key={index} className={`floating-word floating-word-${index}`}>
+      {words.map((word) => (
+        <span key={uuidv4()} className="floating-word">
           {word}
         </span>
       ))}
