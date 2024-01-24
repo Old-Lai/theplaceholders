0b2e188843fa6a793eecb293bb04e1ece7ccccae
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-01-23 18:19:41 -0800

    added appearing animation to title

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 import FloatingWords from "../components/FloatingWords";
 import '../styles/menuPage.sass';
 function MenuPage() {
+  const [title, setTitle] = useState("")
+  const titleText = "The Placeholders </>"
+  useEffect(() => {
+    const titleInterval = setInterval(() => {
+      setTitle((prev)=>{
+        if(prev.length >= titleText.length){
+          document.getElementsByClassName('blinkingCurser')[0].classList.add('hidden')
+          clearInterval(titleInterval)
+        }
+        return titleText.substring(0, prev.length + 1)
+      })
+    }, 120)
+  },[])
   return (
     <>
       <FloatingWords />
       <div className='menuBody'>
-        <h1>The Placeholders</h1>
+        <div className='title-container'>
+          <h1 id='title'>{title}</h1>
+          <span className='blinkingCurser'>|</span>
+        </div>
         <ul className='menuLinks'>
           <li className='chrisButton'><a href="/chris">Chris</a></li>
           <li><a href="/oldlai">Henry</a></li>
